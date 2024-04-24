db0aee8fa55e21eb82ad5684f522b7010089b05b
Het Mamtora <104263376+HetMamtora@users.noreply.github.com>
2024-04-24 22:12:58 +0530

    updated

--- server/routes/upload.js
+++ server/routes/upload.js
@@ -12,7 +12,7 @@ cloudinary.config({
     api_secret: process.env.CLOUD_API_SECRET
 })
 
-router.post('/upload',auth,authAdmin, (req,res) => {
+/*router.post('/upload',auth,authAdmin, (req,res) => {
     try{
         if(!req.files || Object.keys(req.files).length === 0){
             return res.status(400).send({msg:"No Files were Uploaded"})
@@ -43,6 +43,36 @@ router.post('/upload',auth,authAdmin, (req,res) => {
                 url:result.secure_url
             })
         })
+    }
+    catch(err){
+        res.status(500).json({msg:err.message})
+    }
+})*/
+router.post('/upload',auth,authAdmin, (req,res)=> {
+    try{
+         if(!req.files || Object.keys(req.files).length ===0)
+         return res.status(400).send({msg: "No file were uploaded"})
+        console.log(req.files)
+
+        const file = req.files.file;
+        if(file.size > 1024*1024){
+            removeTmp(file.tempFilePath)
+            return res.status(400).json({msg:"Size too large"})
+        } 
+
+        if(file.mimetype !== 'image/jpeg' && file.mimetype !== 'image/png')
+        {
+        removeTmp(file.tempFilePath)
+        return res.status(400).json({msg:"File format is incorrect"})
+        }
+
+         cloudinary.v2.uploader.upload(file.tempFilePath,{folder:'test'},async(err,result) => {
+            if(err) throw err;
+
+            removeTmp(file.tempFilePath)
+
+            res.json({public_id:result.public_id,url:result.secure_url})
+         })
     }
     catch(err){
         res.status(500).json({msg:err.message})
