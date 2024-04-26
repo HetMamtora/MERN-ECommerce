ca23dd130cba911429928d1330874ccef88d8340
Het Mamtora <104263376+HetMamtora@users.noreply.github.com>
2024-04-26 19:41:03 +0530

    Update productCtrl.js

--- server/controllers/productCtrl.js
+++ server/controllers/productCtrl.js
@@ -14,19 +14,34 @@ class APIfeatures{
         excludedFields.forEach(el => delete(queryObj[el]))
 
         let queryStr = JSON.stringify(queryObj)
-        queryStr = queryStr.replace(/\b(gte|gt|lt|lte\regex)\b/g, match => '₹' + match)
+        queryStr = queryStr.replace(/\b(gte|gt|lt|lte\regex)\b/g, match => '$' + match)
 
-        console.log({queryObj,queryStr})
+        this.query.find(JSON.parse(queryStr))
 
         return this
     }
 
     sorting(){
-
+        if(this.queryString.sort){
+            const sortBy = this.queryString.sort.split(',').join('')
+            this.query = this.query.sort(sortBy)
+            console.log(sortBy)
+        }
+        else{
+            this.query = this.query.sort('-createdAt')
+        }
+        
+        return this
     }
 
     pagination(){
+        const page = this.queryString.page * 1||1;
+        const limit = this.queryString.limit * 1||9;
+        const skip = (page-1) * limit;
 
+        this.query = this.query.skip(skip).limit(limit);
+
+        return this
     }
 }
 
@@ -34,7 +49,7 @@ const productCtrl = {
     
     getProducts: async(req,res) => {
         try{
-            const features = new APIfeatures(Products.find(),req.query).filtering()
+            const features = new APIfeatures(Products.find(),req.query).filtering().sorting().pagination()
             const products = await features.query
             
             res.json(products)
