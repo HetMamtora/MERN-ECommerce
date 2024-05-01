8e9174e0d4a7da7bb7a8ce27e22b3f50e78395f4
Het Mamtora <104263376+HetMamtora@users.noreply.github.com>
2024-05-01 18:56:29 +0530

    updated

--- client/src/GlobalState.js
+++ client/src/GlobalState.js
@@ -1,11 +1,25 @@
-import { createContext, useState } from "react";
+import { createContext, useEffect, useState } from "react";
 import ProductAPI from "./api/ProductAPI";
+import axios from "axios";
 
 export const GlobalState = createContext()
 
 export const DataProvider = ({children}) => {
 
     const [token, setToken] = useState(false)
+
+    const refreshToken = async() => {
+        const res = await axios.get('/user/refresh_token')
+        setToken(res.data.accesstoken)
+        //console.log(token)
+    }
+
+    useEffect(() => {
+        const firstLogin = localStorage.getItem('firstLogin')
+        if(firstLogin){
+            refreshToken()
+        }
+    },[])
 
     const state = {
         token: [token,setToken],
