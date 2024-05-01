5519c2d4d53c7cdb63609edc8b85dff33aaf7499
Het Mamtora <104263376+HetMamtora@users.noreply.github.com>
2024-05-01 17:32:47 +0530

    updated

--- client/src/components/mainpages/utils/DetailProducts/DetailProduct.js
+++ client/src/components/mainpages/utils/DetailProducts/DetailProduct.js
@@ -1,5 +1,5 @@
-import React, { useContext, useState } from 'react'
-import { useParams } from 'react-router-dom'
+import React, { useContext, useEffect, useState } from 'react'
+import { Link, useParams } from 'react-router-dom'
 import { GlobalState } from '../../../../GlobalState'
 
 const DetailProduct = () => {
@@ -9,8 +9,39 @@ const DetailProduct = () => {
 
     const [products] = state.productsAPI.products
     const [detailProduct,setDetailProduct] = useState([])
+
+    useEffect(() => {
+        if(params){
+            products.forEach(product => {
+                if(product._id === params.id){
+                    setDetailProduct(product)
+                }
+            });
+        }
+    },[params,products])
+
+    if(detailProduct.length ===0){
+        return null
+    }
+
+    console.log(detailProduct)
+
   return (
-    <div>DetailProduct</div>
+    <div className='detail'>
+        <img src={detailProduct.images.url} alt='' />
+        <div className='box-detail'>
+            <div className='row'>
+                <h2>{detailProduct.title}</h2>
+                <h6>{detailProduct.product_id}</h6>
+            </div>
+
+            <span>₹{detailProduct.price}</span>
+            <p>{detailProduct.description}</p>
+            <p>{detailProduct.content}</p>
+            <p>Sold: {detailProduct.sold}</p>
+            <Link to='/cart' className='cart'>Buy Now</Link>
+        </div>
+    </div>
   )
 }
 
