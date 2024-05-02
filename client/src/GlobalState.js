6563637760eb3234ba2961ac2d12a4c3a65f2ccb
Het Mamtora <104263376+HetMamtora@users.noreply.github.com>
2024-05-02 21:06:49 +0530

    updated

--- client/src/GlobalState.js
+++ client/src/GlobalState.js
@@ -1,4 +1,4 @@
-import { createContext, useEffect, useState } from "react";
+/*import { createContext, useEffect, useState } from "react";
 import ProductAPI from "./api/ProductAPI";
 import axios from "axios";
 import UserAPI from "./api/UserAPI";
@@ -25,6 +25,42 @@ export const DataProvider = ({children}) => {
         token: [token,setToken],
         productsAPI: ProductAPI(),
         userAPI: UserAPI(token)
+    }
+
+    return(
+        <GlobalState.Provider value={state}>
+            {children}
+        </GlobalState.Provider>
+    )
+}*/
+
+import { createContext, useEffect } from "react";
+import ProductAPI from "./api/ProductAPI";
+import { useState } from "react";
+import axios from "axios";
+import UserAPI from "./api/UserAPI";
+
+export const GlobalState = createContext()
+
+export const DataProvider = ({children}) => {
+
+    const [token,setToken] = useState(false)
+
+    const refreshToken = async () => {
+        const res = await axios.get('/user/refresh_token')
+
+        setToken(res.data.accesstoken)
+    }
+
+    useEffect(()=>{
+        const firstLogin = localStorage.getItem('firstLogin')
+        if(firstLogin) refreshToken()
+    },[])
+
+    const state = {
+        token: [token,setToken],
+        productsAPI:ProductAPI(),
+        userAPI:UserAPI(token)
     }
 
     return(
--- client/src/components/mainpages/utils/DetailProducts/DetailProduct.js
+++ client/src/components/mainpages/utils/DetailProducts/DetailProduct.js
@@ -1,4 +1,4 @@
-import React, { useContext, useEffect, useState } from 'react'
+/*import React, { useContext, useEffect, useState } from 'react'
 import { Link, useParams } from 'react-router-dom'
 import { GlobalState } from '../../../../GlobalState'
 
@@ -39,6 +39,48 @@ const DetailProduct = () => {
             <p>Sold: {detailProduct.sold}</p>
             <Link to='/cart' className='cart'>Buy Now</Link>
         </div>
+    </div>
+  )
+}
+
+export default DetailProduct*/
+
+import React, { useContext, useEffect, useState } from 'react'
+import { Link, useParams } from 'react-router-dom'
+import { GlobalState } from '../../../../GlobalState'
+
+const DetailProduct = () => {
+
+    const params = useParams()
+    const state = useContext(GlobalState)
+    const [products] = state.productsAPI.products
+    const [detailProduct,setDetailProduct] = useState([])
+
+    useEffect(()=> {
+        if(params){
+            products.forEach(product => {
+                if(product._id === params.id) setDetailProduct(product)
+            })
+        }
+    },[params,products])
+
+    if(detailProduct.length === 0) return null;
+
+    console.log(detailProduct)
+  return (
+    <div className='detail'>
+      <img src={detailProduct.images.url} alt=''/>
+      <div className='box-detail'>
+        <div className='row'>
+            <h2>{detailProduct.title}</h2>
+            <h6>{detailProduct.product_id}</h6>
+        </div>
+        <span>${detailProduct.price}</span> 
+        <p>{detailProduct.description}</p> 
+        <p>{detailProduct.content}</p>
+        <p>Sold:{detailProduct.sold}</p>
+        <Link to='/cart' className='cart'>Buy Now</Link>
+      </div>
     </div>
   )
 }
