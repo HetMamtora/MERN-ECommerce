240c584621281baac1d7eed10e7efcafe9efab41
Het Mamtora <104263376+HetMamtora@users.noreply.github.com>
2024-05-02 14:56:53 +0530

    updated

--- client/src/components/mainpages/utils/ProductLists/ProductList.js
+++ client/src/components/mainpages/utils/ProductLists/ProductList.js
@@ -2,7 +2,7 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 const ProductList = ({product}) => {
 
-    console.log(product)
+    //console.log(product)
 
   return (
     <div className='product_card'>
--- server/routes/userRouter.js
+++ server/routes/userRouter.js
@@ -7,7 +7,7 @@ const router = express.Router();
 router.post('/register',userCtrl.register)
 router.post('/login',userCtrl.login)
 router.get('/logout', userCtrl.logout);
-router.post('/refresh_token', userCtrl.refreshtoken);
+router.get('/refresh_token', userCtrl.refreshtoken);
 router.get('/information',auth,userCtrl.getUser)
 
 module.exports = router
