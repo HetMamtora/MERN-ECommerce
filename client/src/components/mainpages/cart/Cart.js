cc8fb1428c303555d581f4da2312db17f0b140b1
Het Mamtora <104263376+HetMamtora@users.noreply.github.com>
2024-05-03 18:58:24 +0530

    updated

--- client/src/components/mainpages/cart/Cart.js
+++ client/src/components/mainpages/cart/Cart.js
@@ -1,4 +1,4 @@
-import React, { useContext } from 'react'
+/*import React, { useContext } from 'react'
 import { GlobalState } from '../../../GlobalState'
 import { Link } from 'react-router-dom'
 
@@ -35,6 +35,45 @@ const Cart = () => {
         }
     </div>
   )
+}
+
+export default Cart*/
+
+import React, { useContext } from 'react'
+import { GlobalState } from '../../../GlobalState'
+import { Link } from 'react-router-dom'
+
+const Cart = () => {
+    const state = useContext(GlobalState)
+    const [cart] = state.userAPI.cart
+
+    if(cart.length === 0){
+        return <h2 style={{textAlign:"center",fontSize:"3rem"}}>Cart Empty</h2>
+    }
+
+    return (
+        <div>
+            {cart.map(item => (
+                <div key={item.product._id} className='detail'>
+                    <img src={item.product.images.url} alt=''/>
+      
+                    <div className='box-detail'>
+                        <div className='row'>
+                            <h2>{item.product.title}</h2>
+                            <h6>{item.product.product_id}</h6>
+                        </div>
+        
+                        <span>${item.product.price}</span> 
+                        <p>{item.product.description}</p> 
+                        <p>{item.product.content}</p>
+                        <p>Sold:{item.product.sold}</p>
+        
+                        <Link to='/cart' className='cart'>Buy Now</Link>
+                    </div>
+                </div>
+            ))}
+        </div>
+    )
 }
 
 export default Cart
